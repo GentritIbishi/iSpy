5e1b0f634a52d8fcd4498c3625dd2c836498068d
Gentrit Ibishi <44057937+GentritIbishi@users.noreply.github.com>
2022-04-23 17:52:19 +0200

    iSpy Starting..

---
+++ app/src/main/java/com/gentritibishi/ispy/GetStartedActivity.java
@@ -0,0 +1,39 @@
+package com.gentritibishi.ispy;
+
+import androidx.annotation.ColorInt;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    private Button bt_getStarted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        bt_getStarted=findViewById(R.id.bt_getStarted);
+
+        bt_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Goes to login
+            }
+        });
+
+        bt_getStarted.setOnHoverListener(new View.OnHoverListener() {
+            @Override
+            public boolean onHover(View view, MotionEvent motionEvent) {
+                bt_getStarted.setBackgroundColor(getResources().getColor(R.color.black_low));
+                return true;
+            }
+        });
+    }
+}
